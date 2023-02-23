23594685245195f921aecd80f138b3e7db8ae9b0
Lewis Smith <lewislbssmith212@gmail.com>
2023-02-23 20:15:24 +0000

    optional extras added

--- src/app.js
+++ src/app.js
@@ -10,11 +10,48 @@ app.post('/cats', (req, res) => {
 });
 
 app.get('/cats', (req, res) => {
-    Cat.findAll({where: req.query}).then(cat => res.status(201).json(cat));
+    Cat.findAll({where: req.query})
+    .then(cat => res.status(201).json(cat))
+    .catch(err => {
+        console.error('Error', err);
+        res.status(400).json({ error: 'Bad Request' });
+    });
 });
 
 app.get('/cats/:catId', (req, res) => {
-    Cat.findByPk(req.params.catId).then(cat => res.status(201).json(cat));
+    Cat.findByPk(req.params.catId)
+    .then(cat => res.status(201).json(cat))
+    .catch(err => {
+        console.error('Error', err);
+        res.status(400).json({ error: 'Bad Request' });
+    });
+});
+
+app.patch('/cats/:catId', (req, res) => {
+    Cat.update(req.body, { where: { id: req.params.catId } })
+    .then(cat => res.status(201).json(cat))
+    .catch(err => {
+        console.error('Error', err);
+        res.status(400).json({ error: 'Bad Request' });
+    });
+});
+
+app.delete('/cats/:catId', (req, res) => {
+    Cat.destroy({ where: { id: req.params.catId }})
+    .then(res.sendStatus(201))
+    .catch(err => {
+        console.error('Error', err);
+        res.status(400).json({ error: 'Bad Request' });
+    });
+});
+
+app.patch('/feed/cat/:catId', (req, res) => {
+    Cat.update( { lastFed: new Date() }, {where: { id: req.params.catId} })
+    .then(cat => res.status(201).json(cat))
+    .catch(err => {
+        console.error('Error', err);
+        res.status(400).json({ error: 'Bad Request' });
+    });
 });
 
 module.exports = app;
